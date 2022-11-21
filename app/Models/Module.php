808e307a5ff124c8478745f1c480cb93e181cfdb
Heyscar Romero <heyscarromero@gmail.com>
2022-11-21 20:00:08 -0300

    Se agrega soft deletes a modules

--- app/Models/Module.php
+++ app/Models/Module.php
@@ -4,10 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Module extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $dates = ['deleted_at'];
+
     protected $fillable = [
         'code',
         'name',
