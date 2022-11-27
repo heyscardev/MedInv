f15d35fe6fa9c61cb304a2d44581c80becd21799
heyscar romero <heyscardesarrolladorcenco@gmail.com>
2022-11-27 09:37:08 -0400

    end create

---
+++ database/factories/PathologyFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Pathology>
+ */
+class PathologyFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            "code" =>fake()->unique()->text(25),
+            "name" =>fake()->unique()->text(25)
+        ];
+    }
+}
