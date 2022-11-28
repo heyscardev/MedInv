ede4945b5ee5dead32a59b8b3bc1496622b2d6b5
Heyscar Romero <heyscarromero@gmail.com>
2022-11-28 13:44:21 -0300

    add code to DoctorRequest

--- app/Http/Requests/DoctorRequest.php
+++ app/Http/Requests/DoctorRequest.php
@@ -42,6 +42,7 @@ class DoctorRequest extends FormRequest
     protected function storeRules()
     {
         return [
+            'code'          => ['required', 'max:30','unique:doctors'],
             'nationality'   => ['required', 'in:V,E'],
             'c_i'           => ['required', 'numeric', 'digits_between:0,8', 'unique:doctors'],
             'first_name'    => ['required', 'alpha', 'max:80'],
@@ -58,11 +59,12 @@ class DoctorRequest extends FormRequest
     {
         return [
             'id'            => ['sometimes','required', 'integer', 'exists:doctors'],
+            'code'          => ['sometimes','required', 'max:30', Rule::unique('doctors')->ignore($this->id) ],
             'nationality'   => ['sometimes','required', 'in:V,E'],
-            'c_i'           => ['sometimes','numeric', 'digits_between:0,8', Rule::unique('doctors')->ignore($this->id)],
+            'c_i'           => ['sometimes','numeric', 'digits_between:0,8', Rule::unique('doctors')->ignore($this->id) ],
             'first_name'    => ['sometimes','alpha', 'max:80'],
             'last_name'     => ['sometimes','alpha', 'max:80'],
-            'birth_date'    => ['sometimes','date_format:Y-m-d', 'after:' . $this->nowMinus150years, 'before:' . $this->yesterday],
+            'birth_date'    => ['sometimes','date_format:Y-m-d', 'after:' . $this->nowMinus150years, 'before:' . $this->yesterday ],
             'gender'        => ['sometimes','in:Male,Female'],
             'email'         => ['sometimes','email', 'max:255', Rule::unique('doctors')->ignore($this->id)],
             'phone'         => ['sometimes','nullable', 'max:25'],
