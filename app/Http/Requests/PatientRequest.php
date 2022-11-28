10f235e171198c2cd0865c88a3f14be26556ed4e
Heyscar Romero <heyscarromero@gmail.com>
2022-11-28 13:18:17 -0300

    refactorvalidation from PatientRequest

--- app/Http/Requests/PatientRequest.php
+++ app/Http/Requests/PatientRequest.php
@@ -42,14 +42,15 @@ class PatientRequest extends FormRequest
     protected function storeRules()
     {
         return [
+            'n_history'     => ['required', 'max:30','unique:patients'],
             'nationality'   => ['required', 'in:V,E'],
-            'c_i'           => ['required', 'numeric', 'digits_between:0,8', 'unique:doctors'],
+            'c_i'           => ['required', 'numeric', 'digits_between:0,8', Rule::unique('patients') ],
             'first_name'    => ['required', 'alpha', 'max:80'],
             'last_name'     => ['required', 'alpha', 'max:80'],
             'birth_date'    => ['required', 'date_format:Y-m-d', 'after:' . $this->nowMinus150years, 'before:' . $this->yesterday],
             'gender'        => ['required', 'in:Male,Female'],
             'child'         => ['required'],
-            'email'         => ['required', 'email', 'max:255', 'unique:doctors'],
+            'email'         => ['required', 'email', 'max:255', 'unique:patients'],
             'phone'         => ['nullable', 'max:25'],
             'direction'     => ['nullable', 'max:250'],
         ];
@@ -58,15 +59,16 @@ class PatientRequest extends FormRequest
     protected function updateRules()
     {
         return [
-            'id'            => ['sometimes','required', 'integer', 'exists:doctors'],
+            'id'            => ['sometimes','required', 'integer', 'exists:patients'],
+            'n_history'     => ['sometimes','required', 'max:30', Rule::unique('patients')->ignore($this->id)],
             'nationality'   => ['sometimes','required', 'in:V,E'],
-            'c_i'           => ['sometimes','numeric', 'digits_between:0,8', Rule::unique('doctors')->ignore($this->id)],
+            'c_i'           => ['sometimes','numeric', 'digits_between:0,8', Rule::unique('patients')->ignore($this->id)],
             'first_name'    => ['sometimes','alpha', 'max:80'],
             'last_name'     => ['sometimes','alpha', 'max:80'],
             'birth_date'    => ['sometimes','date_format:Y-m-d', 'after:' . $this->nowMinus150years, 'before:' . $this->yesterday],
             'gender'        => ['sometimes','in:Male,Female'],
             'child'         => ['sometimes','required'],
-            'email'         => ['sometimes','email', 'max:255', Rule::unique('doctors')->ignore($this->id)],
+            'email'         => ['sometimes','email', 'max:255', Rule::unique('patients')->ignore($this->id)],
             'phone'         => ['sometimes','nullable', 'max:25'],
             'direction'     => ['sometimes','nullable', 'max:250'],
         ];
