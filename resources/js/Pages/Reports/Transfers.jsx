b865edd0034b397f3b623747dfdfdaf5c2402712
Heyscar Romero <heyscarromero@gmail.com>
2022-12-08 03:29:53 -0300

    reports

--- resources/js/Pages/Reports/Transfers.jsx
+++ resources/js/Pages/Reports/Transfers.jsx
@@ -1,9 +1,48 @@
 import ReportTemplate from '@/Components/ReportTemplate'
-import { fabClasses, Grid, Typography } from '@mui/material'
+import { Box, fabClasses, Grid, Typography, useTheme } from '@mui/material'
+import { width } from '@mui/system'
 import { format } from 'date-fns'
 import MaterialReactTable from 'material-react-table'
+import { useMemo } from 'react'
 
 export default ({ data, total_rows, report_type, start_date, end_date }) => {
+
+    const { palette } = useTheme();
+
+    const columns = useMemo(
+        () => [
+            {
+                accessorKey: 'id',
+                header: 'ID',
+                maxSize:2,
+            },
+            {
+                accessorKey: 'module_send.name',
+                header: 'Módulo envía',
+                maxSize:90,
+            },
+            {
+                accessorKey: 'module_receive.name',
+                header: 'Módulo recibe',
+                maxSize:90,
+            },
+            {
+                accessorKey: 'user_id',
+                header: 'Usuario Responsable',
+                accessorFn: ({ user }) => `${user.first_name} ${user.last_name}`,
+
+                maxSize:30,
+            },
+            {
+                accessorKey: 'created_at',
+                header: 'Fecha de registro',
+                accessorFn: ({ created_at }) =>
+                  format(new Date(created_at), 'hh:mm:aa dd/MM/yyyy'),
+                maxSize:27,
+            },
+        ]
+    );
+
   return (
     <ReportTemplate {...{ start_date, end_date, nameReport: report_type, orientation: 'landscape' }}>
         <MaterialReactTable
@@ -19,43 +58,42 @@ export default ({ data, total_rows, report_type, start_date, end_date }) => {
           enableTableFooter
           enableStickyHeader
           autoResetExpanded
+          enableRowVirtualization
           data={data}
-          columns={[
-            {
-              accessorKey: 'id',
-              header: 'ID',
-              maxSize:2,
+          columns={columns}
+          renderDetailPanel={({ row }) => (
+            <Box>
+                <table style={{ width: '98%', background:palette.primary.light }}>
+                    <tbody>
+                        { row.original.medicaments.map( (item) => (
+                            <tr key={item.id}>
+                                <td>{item.code}</td>
+                                <td>{item.name}</td>
+                                <td>{item.unit.name}</td>
+                                <td>{item.pivot.quantity}</td>
+                            </tr>
+                        ) )}
+                    </tbody>
+                </table>
+            </Box>
+          )}
+          positionExpandColumn="last"
+          displayColumnDefOptions={{
+            'mrt-row-expand': {
+              minSize: 0,
+              size:2,
+              muiTableHeadCellProps: {
+                align: 'right',
+              },
+              muiTableBodyCellProps: {
+                align: 'right',
+              },
             },
-            {
-              accessorKey: 'module_send.name',
-              header: 'Módulo envía',
-              maxSize:90,
-            },
-            {
-              accessorKey: 'module_receive.name',
-              header: 'Módulo recibe',
-              maxSize:90,
-            },
-            {
-              accessorKey: 'user_id',
-              header: 'Usuario Responsable',
-              accessorFn: ({ user }) => `${user.first_name} ${user.last_name}`,
-
-              maxSize:30,
-            },
-            {
-              accessorKey: 'created_at',
-              header: 'Fecha de registro',
-              accessorFn: ({ created_at }) =>
-                format(new Date(created_at), 'hh:mm:aa dd/MM/yyyy'),
-              maxSize:27,
-            },
-          ]}
+          }}
         />
 
         <Grid container sx={{ marginTop: 3 }}>
             <Grid item xs={6}></Grid>
-
             <Grid item xs={6} display="flex" justifyContent="end">
                 <Typography>
                     Total de transferencias: {total_rows}
